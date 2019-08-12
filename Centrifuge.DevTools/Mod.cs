a99136ad25f30300c137a8dcfac9eb28b8a508f6
Ciastex <Ciastex@users.noreply.github.com>
2019-08-12 14:03:38 +0200

    Added some in-game commands.

--- Centrifuge.DevTools/Mod.cs
+++ Centrifuge.DevTools/Mod.cs
@@ -1,9 +1,10 @@
-﻿using Reactor.API.Attributes;
+﻿using CommandTerminal;
+using Reactor.API.Attributes;
 using Reactor.API.Configuration;
+using Reactor.API.GTTOD;
 using Reactor.API.Interfaces.Systems;
 using Reactor.API.Storage;
 using UnityEngine;
-
 using Logger = Reactor.API.Logging.Logger;
 
 namespace Centrifuge.DevTools
@@ -12,8 +13,13 @@ namespace Centrifuge.DevTools
     public class Mod : MonoBehaviour
     {
         public const string ModID = "com.ciastex.github/Centrifuge.DevTools";
+
         public const string DumpSceneBasicSettingsKey = "DumpSceneBasic";
         public const string DumpSceneDetailedSettingsKey = "DumpSceneDetailed";
+        public const string AddConsoleCommandsSettingsKey = "AddConsoleCommands";
+
+        public const string DumpSceneCommandTrigger = "sc_dump";
+        public const string QuitCommandTrigger = "quit";
 
         private Logger _logger;
         private Settings _settings;
@@ -27,16 +33,48 @@ namespace Centrifuge.DevTools
             _dumper = new Dumper(_fileSystem);
             _logger = new Logger("event");
 
+            GameAPI.TerminalInitialized += GameAPI_TerminalInitialized;
+
             InitSettings();
             SetUpKeyBinds(manager.Hotkeys);
         }
 
+        private void GameAPI_TerminalInitialized(object sender, System.EventArgs e)
+        {
+            Terminal.Shell.AddCommand(DumpSceneCommandTrigger, (args) =>
+            {
+                if (args[0].String == "basic")
+                {
+                    DumpBasic();
+                    Terminal.Log("Basic dump finished.");
+                }
+                else if (args[0].String == "detailed")
+                {
+                    DumpDetailed();
+                    Terminal.Log("Detailed dump finished.");
+                }
+                else
+                {
+                    Terminal.Log("Need 'basic' or 'detailed'.");
+                }
+            }, 1, 1, "Dumps entire object tree in the current scene into a file inside mod's Data directory.");
+
+            Terminal.Shell.AddCommand(QuitCommandTrigger, (args) =>
+            {
+                Application.Quit(0);
+            }, 0, -1, "Quits the game.");
+
+            Terminal.Autocomplete.Register(DumpSceneCommandTrigger);
+            Terminal.Autocomplete.Register(QuitCommandTrigger);
+        }
+
         private void InitSettings()
         {
-            _settings = new Settings("binds");
+            _settings = new Settings("config");
 
             _settings.GetOrCreate(DumpSceneBasicSettingsKey, "LeftControl+F7");
             _settings.GetOrCreate(DumpSceneDetailedSettingsKey, "LeftControl+F8");
+            _settings.GetOrCreate(AddConsoleCommandsSettingsKey, true);
 
             if (_settings.Dirty)
                 _settings.Save();
@@ -47,18 +85,28 @@ namespace Centrifuge.DevTools
             hotkeys.Bind(_settings.GetItem<string>(DumpSceneBasicSettingsKey),
                 () =>
                 {
-                    _logger.Info("Performing basic scene dump...");
-                    _dumper.DumpCurrentScene(false);
+                    DumpBasic();
                 }
             );
 
             hotkeys.Bind(_settings.GetItem<string>(DumpSceneDetailedSettingsKey),
                 () =>
                 {
-                    _logger.Info("Performing detailed scene dump...");
-                    _dumper.DumpCurrentScene(true);
+                    DumpDetailed();
                 }
             );
+        }
+
+        private void DumpBasic()
+        {
+            _logger.Info("Performing basic scene dump...");
+            _dumper.DumpCurrentScene(false);
+        }
+
+        private void DumpDetailed()
+        {
+            _logger.Info("Performing detailed scene dump...");
+            _dumper.DumpCurrentScene(true);
         }
     }
 }
